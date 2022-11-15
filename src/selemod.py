534397941c04d74411b2f360898846611e6044a3
Ryo-Kuzuno <kuzuno1027@gmail.com>
2022-11-16 04:06:29 +0900

    Move component to integrated libarry selemod.py

--- src/selemod.py
+++ src/selemod.py
@@ -788,6 +788,25 @@ class E2S:
     def destroy(self): 
         gpio.cleanup()
 
+class EM_SW:
+    def __init__(self, pin_em_sw): 
+        """
+        Argument
+        ------------------------------------------
+        pin_em_sw : emergency switch signal gpio 
+        """
+        self.pin_em_sw = pin_em_sw
+        GPIO.setup(self.pin_em_sw, GPIO.IN, GPIO.PUD_UP) #switch is connected with pull-up  
+    
+    def read(self): 
+        """switch not pressed => 1  switch pressed => 0"""
+        em_sw_state = GPIO.input(self.pin_em_sw)
+        return em_sw_state   
+
+    def destoy(self): 
+        GPIO.cleanup()
+        sys.exit()
+
 class TWILITE_REMOTE:
     def __init__(self, pin_remote_actuate, pin_remote_stop): 
         """
