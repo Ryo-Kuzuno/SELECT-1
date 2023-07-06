5c7e7f308e3a659dbdd383f3a2b58920717e2371
Ryo-Kuzuno <kuzuno1027@gmail.com>
2023-07-06 23:29:08 +0900

    modify evdev

--- src/test/code_test/test_evdev.py
+++ src/test/code_test/test_evdev.py
@@ -1,6 +1,11 @@
 import evdev
 
 #検知したいデバイスのevent番号を書いてください
+devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+
+for device in devices:
+    print(device.path, device.name, device.phys)
+
 device = evdev.InputDevice('/dev/input/event0')
 
 for event in device.read_loop():
