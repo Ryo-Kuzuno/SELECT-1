5a833f5711893950048cfddb4997e9099fc13394
Ryo-Kuzuno <kuzuno1027@gmail.com>
2023-07-06 23:56:33 +0900

    add tty

---
+++ src/test/code_test/test_tty.py
@@ -0,0 +1,21 @@
+import tty
+import sys
+import termios
+
+def getkey():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+
+    try:
+        tty.setcbreak(fd)
+        key = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+    return key
+
+while True:
+    key = getkey()
+    if key == 'q':
+        print("input: q")
+    # キー入力の処理
