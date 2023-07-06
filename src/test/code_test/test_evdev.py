50cdb769010603c5b2a94ceee8d815d1bdc8bdbe
Ryo-Kuzuno <kuzuno1027@gmail.com>
2023-07-06 22:38:43 +0900

    Add test_evdev

---
+++ src/test/code_test/test_evdev.py
@@ -0,0 +1,14 @@
+import evdev
+
+#検知したいデバイスのevent番号を書いてください
+device = evdev.InputDevice('/dev/input/event1')
+
+for event in device.read_loop():
+    if event.type == evdev.ecodes.EV_KEY:
+        if event.value == 1: #0:UP, 1:DOWN
+            if event.code == evdev.ecodes.KEY_KP1:
+                print('KEY1')
+            if event.code == evdev.ecodes.KEY_KP2:
+                print('KEY2')
+            if event.code == evdev.ecodes.KEY_KP3:
+                print('KEY3')
